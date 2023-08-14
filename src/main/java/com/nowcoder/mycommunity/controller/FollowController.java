4374ada22e164431e3bdefb99fe4bc2d74974b0d
back2childhood <1939371557@qq.com>
2023-08-14 23:46:30 +0800

    show other's list of subscribe and the person being followed

--- src/main/java/com/nowcoder/mycommunity/controller/FollowController.java
+++ src/main/java/com/nowcoder/mycommunity/controller/FollowController.java
@@ -1,22 +1,35 @@
 package com.nowcoder.mycommunity.controller;
 
+import com.nowcoder.mycommunity.entity.Page;
 import com.nowcoder.mycommunity.entity.User;
 import com.nowcoder.mycommunity.service.FollowService;
+import com.nowcoder.mycommunity.service.UserService;
+import com.nowcoder.mycommunity.util.CommunityConstant;
 import com.nowcoder.mycommunity.util.CommunityUtil;
 import com.nowcoder.mycommunity.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Controller
-public class FollowController {
+public class FollowController implements CommunityConstant{
 
     @Autowired
     private FollowService followService;
 
     @Autowired
-    private  HostHolder hostHolder;
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @PostMapping(path = "/follow")
     @ResponseBody
@@ -36,5 +49,64 @@ public class FollowController {
         followService.unfollow(user.getId(), entityType, entityId);
 
         return CommunityUtil.getJSONString(0, "unfollowed");
+    }
+
+    @GetMapping(path = "/followees/{userId}")
+    public String getFollowees(@PathVariable("userId") int userId, Page page, Model model){
+        User user = userService.findUserById(userId);
+        if(user == null){
+            throw new RuntimeException("The user does not exist");
+        }
+
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/followees/" + userId);
+        page.setRows((int) followService.findFolloweeCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<String, Object>> userList = followService.findFollowees(userId, page.getOffset(), page.getLimit());
+        if(userList != null){
+            for (Map<String, Object> map : userList) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+        return "/site/followee";
+    }
+
+    @GetMapping(path = "/followers/{userId}")
+    public String getFollowers(@PathVariable("userId") int userId, Page page, Model model){
+        User user = userService.findUserById(userId);
+        if(user == null){
+            throw new RuntimeException("The user does not exist");
+        }
+        model.addAttribute("user", user);
+
+        page.setLimit(5);
+        page.setPath("/followers/" + userId);
+        page.setRows((int) followService.findFollowerCount(userId, ENTITY_TYPE_USER));
+
+        List<Map<String, Object>> userList = followService.findFollowers(userId, page.getOffset(), page.getLimit());
+        if(userList != null){
+            for (Map<String, Object> map : userList) {
+                User u = (User) map.get("user");
+                map.put("hasFollowed", hasFollowed(u.getId()));
+            }
+        }
+        model.addAttribute("users", userList);
+
+//        System.out.println(userList.size());
+
+        return "/site/follower";
+    }
+
+    private boolean hasFollowed(int userId){
+        User user = hostHolder.getUser();
+        if(user == null){
+            return false;
+        }
+        return followService.hasFollowed(user.getId(), ENTITY_TYPE_USER, userId);
     }
 }
